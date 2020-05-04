b8741cfede2450bf75377aff8e8e91d05e1bf105
Martín Gaitán <gaitan@gmail.com>
2020-05-04 13:45:49 -0300

    bump 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="move-imports",
-    version="0.1",
+    version="0.2",
     description="""Refactor modules to move import statements to the header""",
     long_description=open("README.md").read(),
     long_description_content_type='text/markdown',
